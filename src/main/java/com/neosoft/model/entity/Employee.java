6228dd03dbe2a019f055fce1d31c15a1ef1d7cdc
mauryaumesh <umesh.maurya@neosoftmail.com>
2023-02-16 17:39:42 +0530

    first commit

---
+++ src/main/java/com/neosoft/model/entity/Employee.java
@@ -0,0 +1,32 @@
+package com.neosoft.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Employee {
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @Column(unique = true)
+    private String employeeId;
+
+    private String employeeName;
+
+    private String phoneNo;
+
+
+}
