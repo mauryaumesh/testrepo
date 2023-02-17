bc60264dc67a722b8bb69afa47931fbb74db1e1c
lchaudhari12022000 <lokesh.chaudhari@neosoftmail.com>
2023-02-17 17:02:15 +0530

    DC-123:feature 2 changes annotation added

--- src/main/java/com/neosoft/repository/EmployeeRepo.java
+++ src/main/java/com/neosoft/repository/EmployeeRepo.java
@@ -2,6 +2,8 @@ package com.neosoft.repository;
 
 import com.neosoft.model.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EmployeeRepo extends JpaRepository<Employee,String> {
 }
--- src/main/java/com/neosoft/service/EmployeeService.java
+++ src/main/java/com/neosoft/service/EmployeeService.java
@@ -1,4 +1,8 @@
 package com.neosoft.service;
 
+
+import org.springframework.stereotype.Service;
+
+@Service
 public class EmployeeService {
 }
