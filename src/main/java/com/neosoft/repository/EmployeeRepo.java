0a9e36bfeabfb0ba1d52c77ba207f57205d82c10
mauryaumesh <umesh.maurya@neosoftmail.com>
2023-02-20 13:07:55 +0530

    DC-127: Added some Rest Api

--- src/main/java/com/neosoft/repository/EmployeeRepo.java
+++ src/main/java/com/neosoft/repository/EmployeeRepo.java
@@ -4,6 +4,16 @@ import com.neosoft.model.entity.Employee;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface EmployeeRepo extends MongoRepository<Employee,String> {
+
+    Employee findByEmployeeId(String employeeId);
+
+    Employee findByEmployeeName(String employeeName);
+
+    List<Employee> findAllByEmployeeName(String employeeName);
+
+    void deleteByEmployeeId(String employeeId);
 }
